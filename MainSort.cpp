bb48aa4c4d6bfb138f5d32cd0f3a70ed87f34d9f
kurian7 <70152750+kurian7@users.noreply.github.com>
2020-10-22 19:10:17 +0530

    Update MainSort.cpp

--- MainSort.cpp
+++ MainSort.cpp
@@ -85,7 +85,16 @@ int main(){
     return 0;
 }
 
-
+void bubbleSort(int arr[], int n)  
+{  
+    int i, j;  
+    for (i = 0; i < n-1; i++)      
+      
+    // Last i elements are already in place  
+    for (j = 0; j < n-i-1; j++)  
+        if (arr[j] > arr[j+1])  
+            swap(&arr[j], &arr[j+1]);  
+}  
 void print(int arr[], int arr_size){
     for (int i = 0; i < arr_size; ++i)
         cout << arr[i] << " ";
